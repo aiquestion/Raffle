56f475b5de56fb212bf42e911a8a563459380550
aiquestion <wangxiaofan0529@gmail.com>
2015-09-28 17:45:46 +0800

    runnable vesrion

---
+++ RaffleWeb/App/Home/Home-local.js
@@ -0,0 +1,8 @@
+﻿function saveSetting(raffle) {
+   window.localStorage.setItem("RaffleStorage", JSON.stringify(raffle));
+}
+function loadSetting() {
+   var content = window.localStorage.getItem("RaffleStorage");
+   if (!content) return null;
+   return JSON.parse(content);
+}
---
+++ RaffleWeb/App/Home/Home.js
@@ -0,0 +1,24 @@
+﻿saveSetting = function () { };
+loadSetting = function () { };
+(function () {
+    "use strict";
+
+    // The initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            saveSetting = function (raffle) {
+                Office.context.document.settings.set("RaffleStorage", JSON.stringify(raffle))
+                Office.context.document.settings.saveAsync();
+            };
+            loadSetting = function () {
+                var content = Office.context.document.settings.get("RaffleStorage");
+                if (!content) return null;
+                return JSON.parse(content);
+            };
+            var setting = loadSetting();
+            if (setting) {
+                init_runtime();
+            }
+        });
+    };
+})();
